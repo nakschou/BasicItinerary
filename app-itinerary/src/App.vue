991e4196d59694ff01540b1f29bb6036481691dd
Justin Chou <justintchou@gmail.com>
2023-04-06 15:34:09 -0700

    minor fixes to the routing and to the views

--- app-itinerary/src/App.vue
+++ app-itinerary/src/App.vue
@@ -1,44 +1,52 @@
 <template>
   <v-app id="inspire" theme="dark">
+    //bar on the sides
     <v-navigation-drawer
         expand-on-hover
         rail
         color="indigo"
       >
+      //top icon and logo
         <v-list>
           <v-list-item
             prepend-avatar="/supernature icon.png"
             title="Supernature Travel"
             subtitle="Basic Itinerary"
+            to="/"
           ></v-list-item>
         </v-list>
 
         <v-divider></v-divider>
-
+      //Table option on nav bar
         <v-list density="compact" nav>
           <v-list-item 
           prepend-icon="mdi-view-list" 
           size="large"
-          title="Graph" 
-          value="graph">
+          title="Table" 
+          value="table"
+          to="/table">
+      //Upload option 
           </v-list-item>
           <v-list-item 
           prepend-icon="mdi-arrow-up-bold-box-outline" 
           size="large"
           title="Upload" 
-          value="upload">
+          value="upload"
+          to="/upload">
           </v-list-item>
         </v-list>
       </v-navigation-drawer>
-
+    //Sets up the top bar
     <v-app-bar>
       <v-toolbar-title>
-        Placeholder
+        Itinerary Toolset
       </v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view>
+
+      </router-view>
     </v-main>
   </v-app>
 </template>
